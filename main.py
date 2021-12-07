388779cad1f40c41e873fbac3563f5a333f0844c
Can <66366306+efecanxrd@users.noreply.github.com>
2021-12-07 13:38:20 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,32 @@
+import cv2
+import dlib
+
+cap = cv2.VideoCapture(0)
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_68.dat")
+
+def node():
+    _, frame = cap.read() 
+
+    gray = cv2.cvtColor(src=frame, code=cv2.COLOR_BGR2GRAY) 
+    faces = detector(gray) 
+    for face in faces: 
+        x1 = face.left() 
+        y1 = face.top() 
+        x2 = face.right() 
+        y2 = face.bottom() 
+        landmarks = predictor(image=gray, box=face) 
+        for n in range(0,68): 
+            x = landmarks.part(n).x 
+            y = landmarks.part(n).y 
+            cv2.circle(img=frame, center=(x,y), radius=2, color=(0,255,0),thickness=-1)
+    cv2.imshow(winname="Cam",mat=frame) 
+
+while True:
+    node()
+    if cv2.waitKey(delay=1)== 27: 
+            break
+
+cap.release()
+cv2.destroyAllWindows()
+exit()
